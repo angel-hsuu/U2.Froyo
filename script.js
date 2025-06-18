ea45484bbf15ba13e764b53cd3096cb9e738d6f4
Angel <angelhsu17@gmail.com>
2025-06-18 13:29:04 -0700

    split

---
+++ script.js
@@ -0,0 +1,14 @@
+// prompt user for flavors
+let flavors = prompt("List out the flavors of froyo and use commas to separate:");
+// turn input into an array of flavors using .split()
+const eachFlavors = flavors.split(",");
+// create empty object
+
+// loop through array
+
+// check if key is in object
+
+// if key is NOT in obj, set key and set value of key to 1
+
+// if key IS in object, then increase value by 1
+
