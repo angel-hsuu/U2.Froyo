76fe74f4fa59da12ab3704386d922de3b988d1be
Angel <angelhsu17@gmail.com>
2025-06-18 14:07:26 -0700

    final edits

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const eachFlavors = flavors.split(",");
 function countFlavors(array) {
 const froyo = {};
 // loop through array
-for (const flavor of eachFlavors) {
+for (const flavor of array) {
 // check if key is in object
 const trimFlavor = flavor.trim();
 // if key is NOT in obj, set key and set value of key to 1
   if (trimFlavor in froyo) {
     froyo[trimFlavor] = froyo[trimFlavor] + 1;
-  } 
+  } else {
 // if key IS in object, then increase value by 1
+froyo[trimFlavor] = 1;
+  }
+}
+return froyo;
+}
 
-}
-}
+const froyoCounts = countFlavors(eachFlavors);
+console.table(froyoCounts);
