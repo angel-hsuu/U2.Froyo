b78902adc4e1e978512ddb0b376cf918632df206
Angel <angelhsu17@gmail.com>
2025-06-18 13:33:26 -0700

    pt2

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ let flavors = prompt("List out the flavors of froyo and use commas to separate:"
 // turn input into an array of flavors using .split()
 const eachFlavors = flavors.split(",");
 // create empty object
-
+function countFlavors(array) {
+const froyo = {};
 // loop through array
-
+for (const flavor of eachFlavors) {
 // check if key is in object
-
+const trimFlavor = flavor.trim();
 // if key is NOT in obj, set key and set value of key to 1
-
+  if (trimFlavor in froyo) {
+    froyo[trimFlavor] = froyo[trimFlavor] + 1;
+  } 
 // if key IS in object, then increase value by 1
 
+}
+}
